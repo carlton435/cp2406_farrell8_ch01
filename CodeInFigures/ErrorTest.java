a4f5b171feb2c2c35f4684d60a2b1fc1ba4f1cf0
two name <https://github.com/carlton435>
2021-11-10 18:08:51 +0800

    fix the problem

--- CodeInFigures/ErrorTest.java
+++ CodeInFigures/ErrorTest.java
@@ -1,8 +1,7 @@
 public class ErrorTest
-/*  This class displays a test message 
 {
    public static void main(String[] args)
    {
-      System.out.println("Test");
+      System.out.println("First Java application");
    }
 }
